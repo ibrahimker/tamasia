2e3c2f974c5acccaa4bdb69a6cf7d6fdd232e4f8
Ibrahim <ibrahimker@gmail.com>
2013-07-12 14:16:12 +0700

    Udah bisa google maps
    Signed-off-by: Ibrahim <ibrahimker@gmail.com>

--- apps/Tamasia/android/native/assets/www/default/wlclient/js/checksum.js
+++ apps/Tamasia/android/native/assets/www/default/wlclient/js/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":3920517009,"date":1373601378757,"machine":"LENOVO-ADI"};
-/* Date: Fri Jul 12 10:56:18 ICT 2013 */
+var WL_CHECKSUM = {"checksum":2414572062,"date":1373613288016,"machine":"LENOVO-ADI"};
+/* Date: Fri Jul 12 14:14:48 ICT 2013 */
