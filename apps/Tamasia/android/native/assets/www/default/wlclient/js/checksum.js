d276d87599a118721bf20996b88e67c45ef484cd
Ibrahim <ibrahimker@gmail.com>
2013-07-21 15:10:00 +0700

    Added Icon and data-theme="f"
    Signed-off-by: Ibrahim <ibrahimker@gmail.com>

--- apps/Tamasia/android/native/assets/www/default/wlclient/js/checksum.js
+++ apps/Tamasia/android/native/assets/www/default/wlclient/js/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":4211735943,"date":1374115667836,"machine":"LENOVO-ADI"};
-/* Date: Thu Jul 18 09:47:47 ICT 2013 */
+var WL_CHECKSUM = {"checksum":3914150521,"date":1374393909897,"machine":"LENOVO-ADI"};
+/* Date: Sun Jul 21 15:05:09 ICT 2013 */
